0a0e964992b1020cff1e46cb725a8061183be8ea
Seanjkennedy <68843638+Seanjkennedy@users.noreply.github.com>
2021-06-07 11:15:07 +0100

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,35 @@
+#Valid Parentheses
+
+"""Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.
+The function should return true if the string is valid, and false if it's invalid.
+
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= input.length <= 100
+
+Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters.
+Furthermore, the input string may be empty and/or not contain any parentheses at all.
+ Do not treat other forms of brackets as parentheses (e.g. [], {}, <>)."""
+
+def valid_parentheses(string):
+    count = 0
+    for i in string:
+        if i == "(":
+            count += 1
+        if i == ")":
+            count -= 1
+        if count < 0:
+            return False
+    if count == 0:
+        return True
+    else:
+        return False
+
+print(valid_parentheses("hi())("))
+print(valid_parentheses("hi(hi)()"))
+print(valid_parentheses("hi)("))
+print(valid_parentheses("hi())("))
